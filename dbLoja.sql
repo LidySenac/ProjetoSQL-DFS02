97539f4433745ccd546f660708fe820f8ead3164
LidySenac <lidian.poliveira@senacsp.edu.br>
2024-05-07 17:15:29 -0300

    sql comandos

--- dbLoja.sql
+++ dbLoja.sql
@@ -11,18 +11,59 @@ use dbLoja;
 show database;
 
 -- criando as tabelas no bd
-create table tbUsuarios(
-codUsu int,
-nome varchar(50),
-senha varchar(20)
-);
+
 
 create table tbFuncionarios(
-codFunc int,
+codFunc int not null auto_increment,
+nome varchar(100) not null,
+email varchar(100),
+cpf char(14) not null unique,
+telCel char(10),
+logradouro varchar(100),
+numero char(10),
+cep char(9),
+bairro varchar(100),
+cidade varchar(100),
+estado char(2),
+primary key(codFunc)
+);
+
+create table tbFornecedores(
+codForn int not null auto_increment,
 nome varchar(100),
 email varchar(100),
-cpf char(14),
-telCel char(10)
+cnpj char(16) not null unique,
+primary key(codForn)
+);
+
+create table tbClientes(
+codCli int not null auto_increment,
+nome varchar(100),
+email varchar(100),
+cpf char(14) not null unique,
+primary key(codCli)
+);
+
+create table tbUsuarios(
+codUsu int auto_increment,
+nome varchar(50),
+senha varchar(20),
+codFunc int not null,
+primary key(codUsu),
+foreign key(codFunc) references tbFuncionarios(codFunc)
+);
+
+create table tbProdutos(
+codProd int not null auto_increment,
+descricao varchar(100),
+valor decimal(9,2),
+quantidade int,
+dataEnt date,
+data date,
+hora time,
+codForn int not null,
+primary key(codProd),
+foreign key(codForn) references tbFornecedores(codForn)
 );
 
 -- visualizando as tabelas criadas
@@ -32,13 +73,20 @@ show tables;
 describe tbUsuarios;
 desc tbFuncionarios;
 
--- inserindo registros nas tabelas
-insert into tbUsuarios(codUsu,nome,senha)
-	values(1,'lidian.oliveira','deximbranco');
+insert into tbFuncionarios(nome,email,cpf,telCel,logradouro,numero,cep,bairro,cidade,estado)
+	values('Lidian Paredes de Oliveira','jinx.oliveira@gmail.com','478.856.875-85','98523-8547','Rua Dr. Bueno','345','04750-000','Santo Amaro','Sao Paulo','SP');
 
-insert into tbFuncionarios(codFunc,nome,email,cpf,telCel)
-	values(10,'Lidian Paredes de Oliveira','jinx.oliveira@gmail.com','478.856.875-85','98523-8547');
+insert into tbFuncionarios(nome,email,cpf,telCel,logradouro,numero,cep,bairro,cidade,estado)
+	values('Gaby Linda Maravilhosa Bolos','gaby.lindabolos@gmail.com','475.850.874-90','98622-8544','Rua Da Alegria','145','04751-002','Santo Amaro','Sao Paulo','SP');
+
+	-- inserindo registros nas tabelas
+ insert into tbUsuarios(nome,senha,codFunc)
+	 values('lidian.oliveira','deximbranco',1);
+
+ insert into tbUsuarios(nome,senha,codFunc)
+	 values('gaby.lindabolosa','123456',2);
 
 -- visualizando os registros nos campos das tabelas
-select * from tbUsuarios;
 select * from tbFuncionarios; 
+select * from tbUsuarios;
+
