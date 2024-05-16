30e42fd78569fd7984a88d02e64c2ee9be8e8ad6
LidySenac <lidian.poliveira@senacsp.edu.br>
2024-05-16 17:08:13 -0300

    Insert Into

--- dbLivraria.sql
+++ dbLivraria.sql
@@ -75,3 +75,94 @@ desc tbVenda;
 desc tbItensDaVenda;
 
 desc tbEscreve;
+
+-- cadastrando os registros nas tabelas --
+
+-- TbGenero --
+
+insert into tbGenero(descricao) values('Acao');
+
+insert into tbGenero(descricao) values('Ficcao');
+
+insert into tbGenero(descricao) values('Aventura');
+
+insert into tbGenero(descricao) values('Terror');
+
+insert into tbGenero(descricao) values('Comedia');
+
+insert into tbGenero(descricao) values('Romance');
+
+insert into tbGenero(descricao) values('Documentario');
+
+-- TbAutor --	
+
+insert into tbAutor(nome,email) values('Agatha Christie','agatha.christie@gmail.com');
+
+insert into tbAutor(nome,email) values('Neil Gaiman','neil.gaiman@gmail.com');
+
+insert into tbAutor(nome,email) values('Ceil Friedman','ceil.friedman@gmail.com');
+
+insert into tbAutor(nome,email) values('Lewis Carroll','lewis.carroll@gmail.com');
+
+insert into tbAutor(nome,email) values('Elizabeth Rudnick','elizabeth.rudnick@gmail.com');
+
+insert into tbAutor(nome,email) values('J. M. Barrie','JM.barrie@gmail.com');
+
+insert into tbAutor(nome,email) values('Kristin Cast, P. C. Cast','kristin@gmail.com');
+
+-- TbCliente --	
+
+insert into tbCliente(nome,telefone) values('Catia','94037-3812');
+
+insert into tbCliente(nome,telefone) values('Gaby','94768-2011');
+
+insert into tbCliente(nome,telefone) values('Beatriz','95078-1023');
+
+-- Generos aleatorios --
+
+insert into tbLivro(titulo,preco,estoque,idGenero) values('O voto de Lenobia',27.90,10,5);
+
+insert into tbLivro(titulo,preco,estoque,idGenero) values('Coraline',52.42,15,3);
+
+insert into tbLivro(titulo,preco,estoque,idGenero) values('Peter Pan',39.24,11,2);
+
+insert into tbLivro(titulo,preco,estoque,idGenero) values('Assassinato no Expresso do Oriente',19.90,20,1);
+
+-- TbVendas --
+
+insert into tbVenda(data,total,idCliente) values('2024-05-16',150.50,2);
+
+insert into tbVenda(data,total,idCliente) values('2024-07-06',140.94,3);
+
+insert into tbVenda(data,total,idCliente) values('2024-06-01', 40.00,1);
+
+-- TbItensVenda --
+
+insert into tbItensDaVenda(idVenda,idlivro,quantidade,subtotal) values(2,3,5,150.00);
+
+insert into tbItensDaVenda(idVenda,idlivro,quantidade,subtotal) values(3,1,2,27.90);
+
+insert into tbItensDaVenda(idVenda,idlivro,quantidade,subtotal) values(1,2,1,55.90);
+
+-- TbEscreve --
+
+insert into tbEscreve(idLivro,idAutor) values(3,3);
+
+insert into tbEscreve(idLivro,idAutor) values(2,2);
+
+insert into tbEscreve(idLivro,idAutor) values(1,7);
+
+-- Pesquisar os campos das tabelas --
+select * from tbGenero;
+select * from tbAutor;
+select * from tbCliente;
+select * from tbLivro;
+select * from tbVenda;
+select * from tbEscreve;
+
+-- Tipos de Registros --
+-- Caracter = 'Senac' como campos de caracter(varchar, char, text)
+-- Numérico - 10 ou 10.50 campos (inteiro ou real)
+-- Data = 'ano/mês/dia'campos(date)
+-- Hora = 'hora:minuto:segundo' campos(time)
+-- Booleano = true ou false (não precisa colocar em apóstolo)
